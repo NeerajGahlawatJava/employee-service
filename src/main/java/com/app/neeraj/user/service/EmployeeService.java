ee9d18c2c6029f79b49e55669b0dcc471083e09f
SALMONC4\ngahlawat <neeraj.gahlawat@wundermanthompson.com>
2022-07-05 13:34:17 +0530

    Circuit breaker resilience4j implementation

--- src/main/java/com/app/neeraj/user/service/EmployeeService.java
+++ src/main/java/com/app/neeraj/user/service/EmployeeService.java
@@ -4,6 +4,7 @@ import com.app.neeraj.user.entity.Employee;
 import com.app.neeraj.user.repository.EmployeeRepository;
 import com.app.neeraj.user.vo.DepartmentVO;
 import com.app.neeraj.user.vo.ResponseTemplateVO;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -19,6 +20,9 @@ public class EmployeeService {
     @Autowired
     private RestTemplate restTemplate;
 
+    private static final String EMPLOYEE_SERVICE = "employeeService";
+    Employee user;
+
     public Employee saveUser(Employee employee) {
         log.info("Inside saveUser method of UserService");
         return employeeRepository.save(employee);
@@ -28,12 +32,20 @@ public class EmployeeService {
         return employeeRepository.findByEmpId(empId);
     }
 
+    @CircuitBreaker(name = EMPLOYEE_SERVICE, fallbackMethod = "getDepartment")
     public ResponseTemplateVO getUserWithDepartment(Long empId){
         ResponseTemplateVO responseTemplateVO = new ResponseTemplateVO();
-        Employee user = findByUserId(empId);
+        user = findByUserId(empId);
        DepartmentVO departmentVO =  restTemplate.getForObject("http://DEPARTMENT-SERVICE/departments/"+empId, DepartmentVO.class);
         responseTemplateVO.setUser(user);
         responseTemplateVO.setDepartment(departmentVO);
+        return responseTemplateVO;
+    }
+
+    public ResponseTemplateVO getDepartment(Exception e){
+        ResponseTemplateVO responseTemplateVO = new ResponseTemplateVO();
+        responseTemplateVO.setUser(user);
+        responseTemplateVO.setDepartment(new DepartmentVO());
         return responseTemplateVO;
     }
 }
